0b90558d7a48a5a73ed4deaa4e06eb405f18c70d
siwar-zr <siwarzrelli180959@gmail.com>
2025-02-20 11:58:17 +0100

    ngStyle and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { AdminComponent } from "./components/admin/admin.component";
 import { UserComponent } from './components/user/user.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { NgIfComponent } from "./components/ng-if/ng-if.component";
 import { NgForComponent } from "./components/ng-for/ng-for.component";
 import { NgClassComponent } from "./components/ng-class/ng-class.component";
+import { NgStyleComponent } from "./components/ng-style/ng-style.component";
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent, NgIfComponent, NgForComponent, NgClassComponent],
+  imports: [DataBindingComponent, NgIfComponent, NgForComponent, NgClassComponent, NgStyleComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
